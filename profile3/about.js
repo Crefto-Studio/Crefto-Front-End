4098ff603b90f43b5d6228581bcb8f19e5d04f4d
1701490 <1701490@eng.asu.edu.eg>
2022-04-23 23:35:11 +0200

    3rd

--- profile3/about.js
+++ profile3/about.js
@@ -2,19 +2,51 @@ var user = localStorage.getItem('user_id');
 
 //get user api
 var url = "http://www.api.crefto.studio/api/v1/users/" + user;
-fetch(url)
+window.onload=info()
+function info() {
+	fetch(url)
 
-	.then(response => response.json())
+		.then(response => response.json())
 
-	.then(json => {
-		console.log(json)
-		if (json.status == "success") {
-			//document.getElementById('name').innerHTML = json.data.user.name;
-			var arr = document.querySelectorAll(".user_name");
-			for (let item of arr) {
-				item.innerHTML = json.data.user.name;
-            }
-			document.getElementById('user_image').src = "http://www.api.crefto.studio/img/users/"+json.data.user.photo;
-			document.getElementById('user_mail').innerHTML = json.data.user.email;
-		}
-	})
+		.then(json => {
+			console.log(json)
+			if (json.status == "success") {
+				//document.getElementById('name').innerHTML = json.data.user.name;
+				var arr = document.querySelectorAll(".user_name");
+				for (let item of arr) {
+					item.innerHTML = json.data.user.name;
+				}
+				document.getElementById('user_image').src = "http://www.api.crefto.studio/img/users/" + json.data.user.photo;
+				document.getElementById('user_mail').innerHTML = json.data.user.email;
+			}
+		})
+}
+
+//toggle between menus
+function display_photos() {
+	document.getElementById("about").style.display = "none";
+	document.getElementById("photos").style.display = "block";
+	document.getElementById("friends").style.display = "none";
+	document.getElementById("posts").style.display = "none";
+}
+
+function display_info() {
+	document.getElementById("about").style.display = "block";
+	document.getElementById("photos").style.display = "none";
+	document.getElementById("friends").style.display = "none";
+	document.getElementById("posts").style.display = "none";
+}
+
+function display_friends() {
+	document.getElementById("friends").style.display = "block";
+	document.getElementById("about").style.display = "none";
+	document.getElementById("photos").style.display = "none";
+	document.getElementById("posts").style.display = "none";
+}
+
+function display_posts() {
+	document.getElementById("posts").style.display = "block";
+	document.getElementById("about").style.display = "none";
+	document.getElementById("photos").style.display = "none";
+	document.getElementById("friends").style.display = "none";
+}
